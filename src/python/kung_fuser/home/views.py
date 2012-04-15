d82796efb98cf571356d738d4ca65c407d1a3c74
gwing33 <gerald.leenerts@gmail.com>
2012-04-14 17:57:13 -0700

    running the view through the home module

--- src/python/kung_fuser/home/views.py
+++ src/python/kung_fuser/home/views.py
@@ -2,8 +2,8 @@ from django.template import Context, loader
 from django.http import HttpResponse
 
 def index(request):
-    t = loader.get_template('base.html')
+    t = loader.get_template('Home/index.html')
     
-    c = Context({})
+    c = Context({"my_name": "Gerald"})
     
     return HttpResponse(t.render(c))
