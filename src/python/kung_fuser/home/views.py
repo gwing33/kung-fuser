973aa35346f1663380c89e383489e1bd2a1c736e
gwing33 <gerald.leenerts@gmail.com>
2012-04-16 11:59:35 -0700

    not sure what this is but committing it anyway lol

--- src/python/kung_fuser/home/views.py
+++ src/python/kung_fuser/home/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 def index(request):
     t = loader.get_template('Home/index.html')
-    
     c = Context({"my_name": "Gerald"})
-    
-    return HttpResponse(t.render(c))
+	return HttpResponse(t.render(c))
